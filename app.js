85a8980c35fe91c4419537b5f5a21149d4ab4e1f
bikatot <vladx002@gmail.com>
2022-07-28 13:16:58 +0300

    Create a first iteration of the 'operate' function

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 function add(a, b) {
   return (a + b);
-}
+};
 
 function subtract(a, b) {
   return (a - b);
-}
+};
 
 function multiply(a, b) {
   return (a * b);
-}
+};
 
 
 function divide(a, b) {
   return (a / b);
-}
+};
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      console.log(`${add(firstNumber, secondNumber)}`);      
+      break;
+    case "-":
+      console.log(`${subtract(firstNumber, secondNumber)}`);  
+      break;
+    case "*":
+      console.log(`${multiply(firstNumber, secondNumber)}`);  
+      break;
+    case "/":
+      console.log(`${divide(firstNumber, secondNumber)}`);  
+      break;
+    default: console.log("Something went wrong");
+      break;
+  };
+};
+
+console.log(operate("/", 50, 10));
